2870f78d782b7a7fc8eea909f1655db12c12e4ec
justinkimm17 <183580285+DontUseCheats@users.noreply.github.com>
2026-05-08 22:19:13 -0700

    Add geocoding, zip code endpoint to country code

---
+++ main.py
@@ -0,0 +1,48 @@
+import os
+import requests
+from dotenv import load_dotenv
+from timezonefinder import TimezoneFinder
+
+tf = TimezoneFinder()
+
+load_dotenv()
+
+api_key = os.getenv("WEATHER_API_KEY")
+
+user_location = input("Enter city or zip code: ")
+
+if user_location.isdigit():
+    url = "http://api.openweathermap.org/geo/1.0/zip"
+    params = {
+        "zip": user_location,
+        "appid": api_key
+        }
+else:
+    url = "http://api.openweathermap.org/geo/1.0/direct"
+    params = {
+        "q": user_location,
+        "appid": api_key
+        }
+
+find_user_location = requests.get(url, params=params)
+
+if user_location.isdigit():
+    location = find_user_location.json()
+else:
+    location = find_user_location.json()[0]
+
+lat = location["lat"]
+lon = location["lon"]
+
+response = requests.get("https://api.openweathermap.org/data/2.5/weather", params={
+    "lat": lat,
+    "lon": lat,
+    "appid": api_key
+})
+
+data = response.json()
+timezonenumber = data["timezone"]
+timezone_name = tf.timezone_at(lat=lat, lng=lon)
+
+
+print(timezone_name)
