86b442dc99464eae058e049ceeaad8e19b3d967f
justinkimm17 <183580285+DontUseCheats@users.noreply.github.com>
2026-05-09 00:43:33 -0700

    Refactor weather logic into helper functions

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 from timezonefinder import TimezoneFinder
 
 app = Flask(__name__)
+load_dotenv()
+api_key = os.getenv("WEATHER_API_KEY")
 
 # homepage route
 @app.route("/")
@@ -12,51 +14,53 @@ def index():
     return render_template("index.html")
 
 # inputted location route
-@app.route("/weather")
+@app.route("/weather", methods=["POST"])
 def weather():
     user_location = request.form["user_location"]
-    return render_templates("weather.html", temperature = current_temperature)
+    url, params = set_city_or_zip(user_location)
+    lat, lon = get_location(url, params, user_location)
+    current_temperature, timezone_name = get_weather(lat, lon)
+    return render_template("weather.html", temperature = current_temperature)
 
-tf = TimezoneFinder()
 
-load_dotenv()
+def set_city_or_zip(user_location):
+    if user_location.isdigit():
+        url = "http://api.openweathermap.org/geo/1.0/zip"
+        params = {
+            "zip": user_location,
+            "appid": api_key
+            }
+    else:
+        url = "http://api.openweathermap.org/geo/1.0/direct"
+        params = {
+            "q": user_location,
+            "appid": api_key
+            }
+    return url, params
 
-api_key = os.getenv("WEATHER_API_KEY")
+def get_location(received_url, params, received_user_location):
+    find_user_location = requests.get(received_url, params=params)
 
-if user_location.isdigit():
-    url = "http://api.openweathermap.org/geo/1.0/zip"
-    params = {
-        "zip": user_location,
+    if received_user_location.isdigit():
+        location = find_user_location.json()
+    else:
+        location = find_user_location.json()[0]
+    return location["lat"], location["lon"]
+
+def get_weather(lat, lon):
+    response = requests.get("https://api.openweathermap.org/data/2.5/weather", params={
+        "lat": lat,
+        "lon": lon,
+        "units": "imperial",
         "appid": api_key
-        }
-else:
-    url = "http://api.openweathermap.org/geo/1.0/direct"
-    params = {
-        "q": user_location,
-        "appid": api_key
-        }
+    })
 
-find_user_location = requests.get(url, params=params)
+    data = response.json()
 
-if user_location.isdigit():
-    location = find_user_location.json()
-else:
-    location = find_user_location.json()[0]
-
-lat = location["lat"]
-lon = location["lon"]
-
-response = requests.get("https://api.openweathermap.org/data/2.5/weather", params={
-    "lat": lat,
-    "lon": lon,
-    "units": "imperial",
-    "appid": api_key
-})
-
-data = response.json()
-
-current_temperature = data["main"]["temp"]
-timezone_name = tf.timezone_at(lat=lat, lng=lon)
+    tf = TimezoneFinder()
+    current_temperature = data["main"]["temp"]
+    timezone_name = tf.timezone_at(lat=lat, lng=lon)
+    return current_temperature, timezone_name
 
 
 if __name__ == "__main__":
